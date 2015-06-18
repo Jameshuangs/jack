5e0bd80e8062959f702aaf6203f804158cef89c1
cclin <s1012554@pu.edu.tw>
2015-06-18 11:05:53 +0800

    ok jackk

--- src/com/example/money2/ExIntent02Activity.java
+++ src/com/example/money2/ExIntent02Activity.java
@@ -7,13 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import com.example.money2.R;
-//i am in 6 17
+//i am in jackk
 public class ExIntent02Activity extends Activity {
 
-// i am in jack
-// i konw
-
-	//j
 
 
 	@Override
