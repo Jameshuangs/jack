4bb9e7a3eb136f16e7c597641c055c52d7c9e092
cclin <s1012554@pu.edu.tw>
2015-09-03 11:48:51 +0800

    0903

--- src/com/example/money2/ExIntent02Activity.java
+++ src/com/example/money2/ExIntent02Activity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import com.example.money2.R;
-//i am in jack 0902////123
+//i am james
 public class ExIntent02Activity extends Activity {
 
 
